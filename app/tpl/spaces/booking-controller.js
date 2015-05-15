3c6b2aa84ca8ce72b18ee5859295e5784b91a6c3
jloyolask8 <jonathan@godesk.cl>
2015-05-15 10:08:36 -0400

    wips

--- app/tpl/spaces/booking-controller.js
+++ app/tpl/spaces/booking-controller.js
@@ -4,76 +4,65 @@
 // signin controller
 
 app.controller('BookingController', function (auth, $scope, $state, $location, $stateParams, SpacesRS) {
-    $scope.test = '';
+
+    $scope.initDate = new Date();
+    $scope.format = 'yyyy/MM/dd HH:mm';
+
     $scope.selectedSpace = {};
     //space/venue type
-    
-    $scope.today = function() {
-      $scope.dt = new Date();
-    };
-    $scope.today();
-    
-    $scope.clear = function () {
-      $scope.dt = null;
-    };
-    
-    $scope.open = function($event) {
-      $event.preventDefault();
-      $event.stopPropagation();
 
-      $scope.opened = true;
+    $scope.newReservationObj = {};
+
+    loadParams();
+
+    function loadParams() {
+        if ($stateParams.dateSelected) {
+            $scope.newReservationObj.startDateTime = $stateParams.dateSelected;
+        } else {
+            $scope.today();
+        }
+
+        if ($stateParams.spaceId) {
+            SpacesRS.getById($stateParams.spaceId).then(
+                    function (v) {
+                        $scope.newReservationObj.space = v;
+                    },
+                    function (err) {
+                        alert('error:' + err);
+                    }
+            );
+        }
+    }
+
+    $scope.today = function () {
+        $scope.newReservationObj.startDateTime = new Date();
+    };
+
+
+    $scope.clear = function () {
+        $scope.newReservationObj.startDateTime = null;
+    };
+
+    $scope.open = function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
     };
 
     $scope.dateOptions = {
-      formatYear: 'yy',
-      startingDay: 1,
-      class: 'datepicker'
+        formatYear: 'yyyy',
+        startingDay: 1,
+        class: 'datepicker'
     };
 
-    $scope.initDate = new Date('2016-15-20');
-    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd HH:mm', 'dd.MM.yyyy', 'shortDate'];
-    $scope.format = $scope.formats[1];
-    
-     $scope.mytime = new Date();
 
-    $scope.hstep = 1;
-    $scope.mstep = 15;
 
-    $scope.optionsTime = {
-      hstep: [1, 2, 3],
-      mstep: [1, 5, 10, 15, 25, 30]
-    };
 
-    $scope.ismeridian = true;
-    $scope.toggleModeTime = function() {
-      $scope.ismeridian = ! $scope.ismeridian;
-    };
-
-    $scope.updateTime = function() {
-      var d = new Date();
-      d.setHours( 14 );
-      d.setMinutes( 0 );
-      $scope.mytime = d;
-    };
-
-    $scope.changedTime = function () {
-      //console.log('Time changed to: ' + $scope.mytime);
-    };
-
-    $scope.clearTime = function() {
-      $scope.mytime = null;
-    };
 
     console.log('hello from BookingController');
 
-    SpacesRS.getById($stateParams.spaceId).then(
-            function (v) {
-                $scope.selectedSpace = v;
-            },
-            function (err) {
-                alert('error:' + err);
-            }
-    );
+
 
     $scope.submit = function () {
 //        window.location.href = e.href("app.page.search", {venuesSearchText: $scope.venuesSearchInputText}, {reload: !0})
